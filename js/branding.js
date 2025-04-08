550a060593df7f13c8d217eaf8f65da9dcc186bf
tim <timstruik0711@gmail.com>
2025-04-08 18:45:41 +0200

    Update branding.js
    Signed-off-by: tim <timstruik0711@gmail.com>

--- js/branding.js
+++ js/branding.js
@@ -24,24 +24,20 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
-document.addEventListener("DOMContentLoaded", function() {
-    const lightboxLinks = document.querySelectorAll('[data-lightbox="gallery"]');
+  function openLightbox(element) {
+    const lightbox = document.getElementById('lightbox');
+    const lightboxImg = document.getElementById('lightbox-img');
+    const scrollPosition = window.scrollY; // Haal de huidige scrollpositie op
 
-    lightboxLinks.forEach(link => {
-        link.addEventListener("click", function() {
-            document.body.classList.add('lightbox-open');
-        });
-    });
+    lightboxImg.src = element.src;
+    lightbox.classList.add('show');
 
+    // Dynamisch de top-positie van de lightbox aanpassen op basis van de scrollpositie
+    lightbox.style.top = (scrollPosition + 10) + "px"; // 10px boven de scrollpositie
+}
 
-    const closeButton = document.querySelector('.lightbox-close'); 
-    if (closeButton) {
-        closeButton.addEventListener("click", function() {
-            document.body.classList.remove('lightbox-open');
-        });
-    }
-});
-lightbox.option({
-  'resizeDuration': 200,
-  'wrapAround': true
-});
+function closeLightbox() {
+    const lightbox = document.getElementById('lightbox');
+    lightbox.classList.remove('show');
+}
+
